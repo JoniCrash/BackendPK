5c23b3cc43f41a089913cf2607c164d8247e98fc
Jonicrash <jonicrash8@gmail.com>
2025-04-28 05:30:19 +0700

    W

--- database/android/pelanggan/profil_bayar.php
+++ database/android/pelanggan/profil_bayar.php
@@ -1,48 +1,37 @@
 <?php
 include('../../../conf/config.php');
-// header('Content-Type: application/json');
+
 // Pastikan semua input tersedia
-if (isset($_POST['id_pembayaran'])) {
-    $id_pembayaran = $_POST['id_pembayaran'];
+if (isset($_POST['id_tagihan'])) {
+    $id_tagihan = $_POST['id_tagihan'];
 
+    // Validasi input kosong
+    if (empty($id_tagihan)) {
+        echo json_encode(["status" => "error", "message" => "ID tagihan harus diisi."]);
+        exit();
+    }
 
-    // Proses verifikasi ke database
-    $sql = mysqli_query($koneksi, "SELECT * FROM tb_pembayaran WHERE id_pembayaran = '$id_pembayaran'");
+    // Query untuk mengambil data tagihan berdasarkan id_tagihan
+    $query = mysqli_query(
+        $koneksi,
+        "SELECT * FROM tb_pembayaran WHERE id_tagihan = '$id_tagihan'"
+    );
 
-    if (mysqli_num_rows($sql) > 0) {
-        $pembayaran = mysqli_fetch_assoc($sql);
+    if (mysqli_num_rows($query) > 0) {
+        $pembayaran = [];
+
+        // Ambil semua data tagihan
+        while ($row = mysqli_fetch_assoc($query)) {
+            $pembayaran[] = $row;
+        }
+
         echo json_encode([
             "status" => "success",
-            "message" => "Login berhasil",
-            "data" => [
-                "id_pembayaran" => $pembayaran['id_pembayaran'],
-                "id_tagihan" => $pembayaran['id_tagihan'],
-                "Nama_Lengkap" => $pembayaran['Nama_Lengkap'],
-                "bukti_pembayaran" => $pembayaran['bukti_pembayaran'],
-                "periode" => $pembayaran['periode'],
-                "status" => $pembayaran['status']
-
-
-
-                // "Nama_Lengkap" => $pelanggan['Nama_Lengkap'],
-                // "nik" => $pelanggan['Nomor_Identitas_KTP'],
-                // "alamat" => $pelanggan['Alamat_Pemasangan'],
-                // "provinsi" => $pelanggan['provinsi'],
-                // "kota" => $pelanggan['kota'],
-                // "latitude" => $pelanggan['latitude'],
-                // "longitude" => $pelanggan['longitude'],
-                // "email" => $pelanggan['Email'],
-                // "nohp1" => $pelanggan['Nomor_Hp_1'],
-                // "nohp2" => $pelanggan['Nomor_Hp_2'],
-                // "idpaket" => $pelanggan['id_paket'],
-                // "namapaket" => $pelanggan['nama_paket'],
-                // "status" => $pelanggan['Status'],
-                // "fotoktp" => $pelanggan['Foto_KTP'],
-                // "fotorumah" => $pelanggan['Foto_Depan_Rumah']
-            ]
+            "message" => "Data tagihan ditemukan.",
+            "data" => $pembayaran
         ]);
     } else {
-        echo json_encode(["status" => "error", "message" => "ID pembayaran tidak ditemukan."]);
+        echo json_encode(["status" => "error", "message" => "Tidak ada tagihan untuk ID tagihan ini."]);
     }
 } else {
     echo json_encode(["status" => "error", "message" => "Input tidak lengkap."]);
