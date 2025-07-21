9b770e6f61da1fb8252855f89d1c900ffde537d8
JoniCrash <jonicrash8@gmail.com>
2025-07-21 09:56:19 +0700

    LKLKLK

--- database/data/req_ubah_paket.php
+++ database/data/req_ubah_paket.php
@@ -26,97 +26,63 @@
                   </table>
                 </form>
               <?php
-              $no = 0; 
-              if (isset($_POST['filter'])) {
-                $dari_tgl = mysqli_real_escape_string($koneksi, $_POST['dari_tgl']);
-                $sampai_tgl = mysqli_real_escape_string($koneksi, $_POST['sampai_tgl']);
-                $query = mysqli_query($koneksi, "
-                  SELECT req.*, p.Nama_Lengkap, pk.nama_paket, pk.kecepatan
-                  FROM tb_req_ubah_paket req
-                  INNER JOIN tb_pelanggan p ON req.id_pelanggan = p.id_pelanggan
-                  INNER JOIN tb_paket pk ON req.id_paket = pk.id_paket
-                  WHERE req.di_buat_pada BETWEEN '$dari_tgl' AND '$sampai_tgl'
-                  ORDER BY req.di_buat_pada DESC
-                ");
-              } else {
-                $query = mysqli_query($koneksi, "
-                  SELECT req.*, p.Nama_Lengkap, pk.nama_paket, pk.kecepatan
-                  FROM tb_req_ubah_paket req
-                  INNER JOIN tb_pelanggan p ON req.id_pelanggan = p.id_pelanggan
-                  INNER JOIN tb_paket pk ON req.id_paket = pk.id_paket
-                  ORDER BY req.di_buat_pada DESC
-                ");
-              }
-              while ($paket = mysqli_fetch_array($query)) {
-                $no++;
-              ?>
-              <tr>
-                <td><?php echo $no ?></td>
-                <td><?php echo $paket['id_pelanggan']; ?></td>
-                <td><?php echo $paket['Nama_Lengkap']; ?></td>
-                <td><?php echo $paket['nama_paket']; ?></td>
-                <td><?php echo $paket['kecepatan']; ?></td>
-                <td>
-                  <?php echo ucfirst($paket['status']); ?>
-                  <br>
-                  <small><?php echo $paket['di_buat_pada']; ?></small>
-                </td>
-              </tr>
-              <?php } ?>
+$no = 0;
 
-              <br><br>
-                <table id="example1" class="table table-bordered table-striped" data-title="Laporan Data Request Ubah Paket">
-                  <thead>
-                  <tr>
-                    <th>No</th>
-                    <th>ID Pelanggan</th>
-                    <th>Nama Pelanggan</th>
-                    <th>Paket Diajukan</th>
-                    <th>Kecepatan</th>
-                    <th>Status</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <?php
-                    $no = 0; 
-                    if (isset($_POST['filter'])) {
-                      $dari_tgl = mysqli_real_escape_string($koneksi,$_POST ['dari_tgl']) ;
-                      $sampai_tgl = mysqli_real_escape_string($koneksi,$_POST ['sampai_tgl']) ;
-                      $query = mysqli_query($koneksi, "
-                      SELECT p.*, 
-                             pj.*, 
-                             pk.*
-                      FROM tb_pelanggan p
-                      INNER JOIN tb_pengajuan pj ON p.id_pengajuan = pj.id_pengajuan
-                      INNER JOIN paket pk ON p.id_paket = pk.id_paket
-                      WHERE pj.dibuat_pada_ BETWEEN '$dari_tgl' AND '$sampai_tgl'
-                    ");
-                    }else{
-                    $query = mysqli_query($koneksi, "
-                      SELECT p.*, 
-                             pj.*, 
-                             pk.*
-                      FROM tb_pelanggan p
-                      INNER JOIN tb_pengajuan pj ON p.id_pengajuan = pj.id_pengajuan
-                      INNER JOIN paket pk ON p.id_paket = pk.id_paket
-                  ");
-                    }
-                    while($paket = mysqli_fetch_array($query)){
-                      $no++
-                    ?>
-                  <tr>
-                    <td width = 5%><?php echo $no?></td>
-                    <td><?php echo $paket ['id_pelanggan'];?></td>
-                    <td><?php echo $paket ['Nama_Lengkap'];?></td>
-                    <td><?php echo $paket ['nama_paket'];?></td>
-                    <td><?php echo $paket ['kecepatan'];?></td>
-                    <td class="noExport">
-                    <a href="index.php?page=ubah-paket-pelanggan&id=<?php echo $paket ['id_pelanggan'];?>" class="btn btn-sm btn-success">Ubah Paket</a>
-                    </td>
-                  </tr>
-                  <?php }?>
-                  </tbody>
-                </table>
+if (isset($_POST['filter'])) {
+  $dari_tgl = mysqli_real_escape_string($koneksi, $_POST['dari_tgl']);
+  $sampai_tgl = mysqli_real_escape_string($koneksi, $_POST['sampai_tgl']);
+  $query = mysqli_query($koneksi, "
+    SELECT req.*, pj.Nama_Lengkap, pk.nama_paket, pk.kecepatan
+    FROM tb_req_ubah_paket req
+    INNER JOIN tb_pelanggan p ON req.id_pelanggan = p.id_pelanggan
+    INNER JOIN tb_pengajuan pj ON p.id_pengajuan = pj.id_pengajuan
+    INNER JOIN paket pk ON req.id_paket = pk.id_paket
+    WHERE req.di_buat_pada BETWEEN '$dari_tgl' AND '$sampai_tgl'
+    ORDER BY req.di_buat_pada DESC
+  ");
+} else {
+  $query = mysqli_query($koneksi, "
+    SELECT req.*, pj.Nama_Lengkap, pk.nama_paket, pk.kecepatan
+    FROM tb_req_ubah_paket req
+    INNER JOIN tb_pelanggan p ON req.id_pelanggan = p.id_pelanggan
+    INNER JOIN tb_pengajuan pj ON p.id_pengajuan = pj.id_pengajuan
+    INNER JOIN paket pk ON req.id_paket = pk.id_paket
+    ORDER BY req.di_buat_pada DESC
+  ");
+}
+?>
+
+<br><br>
+<table id="example1" class="table table-bordered table-striped" data-title="Laporan Data Request Ubah Paket">
+  <thead>
+    <tr>
+      <th>No</th>
+      <th>ID Pelanggan</th>
+      <th>ID Pelanggan</th>
+      <th>Nama Pelanggan</th>
+      <th>Paket Diajukan</th>
+      <th>Kecepatan</th>
+      <th>Status</th>
+      <th>Tanggal Request</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php while ($paket = mysqli_fetch_array($query)) { $no++; ?>
+      <tr>
+        <td><?php echo $no; ?></td>
+        <td><?php echo $paket['id_request']; ?></td>
+        <td><?php echo $paket['id_pelanggan']; ?></td>
+        <td><?php echo $paket['Nama_Lengkap']; ?></td>
+        <td><?php echo $paket['nama_paket']; ?></td>
+        <td><?php echo $paket['kecepatan']; ?></td>
+        <td><?php echo ucfirst($paket['status']); ?></td>
+        <td><?php echo $paket['di_buat_pada']; ?></td>
+    <td><a href="index.php?page=ubah-paket-pelanggan&id=<?php echo $paket['id_pelanggan']; ?>" class="btn btn-sm btn-success mt-1">Ubah Paket</a></td>
+      </tr>
+    <?php } ?>
+  </tbody>
+</table>
+
               </div>
               <!-- /.card-body -->
             </div>
