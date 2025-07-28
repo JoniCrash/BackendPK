18a9b4633a4bc90853778e8f88de83ea3558db90
JoniCrash <iwan@comet.net.id>
2025-07-29 04:01:10 +0700

    #

--- conf/config.php
+++ conf/config.php
@@ -1,8 +1,8 @@
 <?php
 $host		= "https://eyehot.store/"; // sesuaikan alamat server anda
-$username	= "u564550804_iwan"; // sesuaikan user web server anda
+$username	= "iwan"; // sesuaikan user web server anda
 $password	= "Iwan123???"; // sesuaikan password web server anda
-$database	= "u564550804_cometservice"; // sesuaikan database web server anda
+$database	= "cometservice"; // sesuaikan database web server anda
 $koneksi = mysqli_connect($host,$username,$password,$database);
 // Mengecek Koneksi
 if(!$koneksi){
