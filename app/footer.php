717fcedf288ba4d74f581ae8db52ef9dd275adde
Jonicrash <jonicrash8@gmail.com>
2025-07-30 00:26:44 +0700

    F

--- app/footer.php
+++ app/footer.php
@@ -402,7 +402,7 @@ function ubahStatusPembayaran(id_pembayaran, Status) {
   console.log("Mengirim status:", id_pembayaran, Status); // Debug log untuk mengecek data yang dikirim
   
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://localhost/database/update/update_status_pembayaran.php", true);
+  xhr.open("POST", "https://localhost/backendpk/database/update/update_status_pembayaran.php", true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   
 xhr.onreadystatechange = function () {
