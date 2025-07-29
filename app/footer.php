c2fe47db124eb21292563f592945780f9cf15cea
Jonicrash <jonicrash8@gmail.com>
2025-07-30 00:42:45 +0700

    J

--- app/footer.php
+++ app/footer.php
@@ -335,7 +335,7 @@ function setPaketID() {
   console.log("Mengirim status:", id_pelanggan, paket, kecepatan, id_paket); // Debug log untuk mengecek data yang dikirim
   
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://localhost/database/update/update_paket_pelanggan.php", true);
+  xhr.open("POST", "https://eyehot.store/database/update/update_paket_pelanggan.php", true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   
   xhr.onreadystatechange = function () {
@@ -358,7 +358,7 @@ function ubahStatusPelanggan(id_pelanggan, Status) {
   console.log("Mengirim status:", id_pelanggan, Status); // Debug log untuk mengecek data yang dikirim
   
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://localhost/database/update/update_status_pelanggan.php", true);
+  xhr.open("POST", "https://eyehot.store/database/update/update_status_pelanggan.php", true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   
   xhr.onreadystatechange = function () {
@@ -402,7 +402,7 @@ function ubahStatusPembayaran(id_pembayaran, Status) {
   console.log("Mengirim status:", id_pembayaran, Status); // Debug log untuk mengecek data yang dikirim
   
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://localhost/backendpk/database/update/update_status_pembayaran.php", true);
+  xhr.open("POST", "https://eyehot.store/database/update/update_status_pembayaran.php", true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   
 xhr.onreadystatechange = function () {
