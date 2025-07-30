3a546d02a55a2e8382113039c80a08c151a5b526
Jonicrash <jonicrash8@gmail.com>
2025-07-31 00:52:21 +0700

    hg

--- database/data/data_pelanggan.php
+++ database/data/data_pelanggan.php
@@ -33,7 +33,11 @@
                 }
               ?>
               <br><br>
-                <table id="example1" class="table table-bordered table-striped" data-title="Laporan Data Pelanggan">
+                <table id="example1" class="table table-bordered table-striped"
+                  data-title="Laporan Data Pelanggan"
+                  data-dari="<?= isset($_POST['filter']) ? htmlspecialchars($_POST['dari_tgl']) : '' ?>"
+                  data-sampai="<?= isset($_POST['filter']) ? htmlspecialchars($_POST['sampai_tgl']) : '' ?>">
+
                   <thead>
                   <tr>
                     <th>No</th>
