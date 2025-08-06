511dd2ff9d0f627c6f51c39355a921d782062050
Jonicrash <jonicrash8@gmail.com>
2025-08-07 02:06:22 +0700

    nnn

--- database/update/update_pelanggan.php
+++ database/update/update_pelanggan.php
@@ -2,39 +2,36 @@
 include('../../conf/config.php');
 
 // Ambil data dari form
-$Nama_Lengkap = $_POST['lama_lengkap'];
-$Nomor_Identitas_KTP = $_POST['nik'];
-$Alamat_Pemasangan = $_POST['alamat_pemasangan'];
+$id_pelanggan = $_POST['id_pelanggan'];
+$nama_lengkap = $_POST['nama_lengkap'];
+$nik = $_POST['nik'];
+$alamat_pemasangan = $_POST['alamat_pemasangan'];
 $latitude = $_POST['latitude'];
 $longitude = $_POST['longitude'];
-$Email =  $_POST['email'];
-$Nomor_Hp_1 = $_POST['nomor_hp_1'];
-$Nomor_Hp_2 = $_POST['nomor_hp_2'];
+$email = $_POST['email'];
+$nomor_hp_1 = $_POST['nomor_hp_1'];
+$nomor_hp_2 = $_POST['nomor_hp_2'];
 $id_paket = $_POST['id_paket'];
 $nama_paket = $_POST['nama_paket'];
-$id_pelanggan = $_POST['id_pelanggan']; // Pastikan variabel ini didefinisikan
 
-// Query untuk update data
+// Query update
 $query = "UPDATE tb_pelanggan SET 
-          Nama_Lengkap = '$Nama_Lengkap', 
-          Nomor_Identitas_KTP = '$Nomor_Identitas_KTP', 
-          Alamat_Pemasangan = '$Alamat_Pemasangan', 
+          Nama_Lengkap = '$nama_lengkap', 
+          Nomor_Identitas_KTP = '$nik', 
+          Alamat_Pemasangan = '$alamat_pemasangan', 
           latitude = '$latitude', 
           longitude = '$longitude', 
-          Email = '$Email', 
-          Nomor_Hp_1 = '$Nomor_Hp_1', 
-          Nomor_Hp_2 = '$Nomor_Hp_2', 
+          Email = '$email', 
+          Nomor_Hp_1 = '$nomor_hp_1', 
+          Nomor_Hp_2 = '$nomor_hp_2', 
           id_paket = '$id_paket', 
           nama_paket = '$nama_paket' 
-          WHERE id_pelanggan='$id_pelanggan'";
+          WHERE id_pelanggan = '$id_pelanggan'";
 
 // Eksekusi query
 if (mysqli_query($koneksi, $query)) {
-    echo json_encode(["success" => true, "message" => "Data pelanggan berhasil diperbarui."]);
+    echo "<script>alert('Data pelanggan berhasil diperbarui.'); window.location.href = '../admin/index.php?page=data-pelanggan';</script>";
 } else {
-    echo json_encode(["success" => false, "message" => "Gagal memperbarui pelanggan: " . mysqli_error($koneksi)]);
+    echo "<script>alert('Gagal memperbarui data: " . mysqli_error($koneksi) . "'); history.back();</script>";
 }
-
-// Redirect setelah menampilkan pesan
-echo "<script>window.location.href = 'index.php?page=data-pelanggan';</script>";
 ?>
