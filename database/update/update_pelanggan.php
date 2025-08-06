78d76bc0051d830bcaf8f679094824b1961357a0
Jonicrash <jonicrash8@gmail.com>
2025-08-07 03:26:19 +0700

    LLL

--- database/update/update_pelanggan.php
+++ database/update/update_pelanggan.php
@@ -70,33 +70,33 @@ if (!empty($set_clause)) {
 }
 
 
-$Nama_Lengkap = $_POST['nama_lengkap'];
-$Nomor_Identitas_KTP = $_POST['nik'];
-$Alamat_Pemasangan = $_POST['alamat_pemasangan'];
-$latitude = $_POST['latitude'];
-$longitude = $_POST['longitude'];
-$Email =  $_POST['email'];
-$Nomor_Hp_1 = $_POST['no_hp1'];
-$Nomor_Hp_2 = $_POST['no_hp2'];
-$id_paket = $_POST['id_paket'];
-$nama_paket = $_POST['nama_paket'];
+// $Nama_Lengkap = $_POST['nama_lengkap'];
+// $Nomor_Identitas_KTP = $_POST['nik'];
+// $Alamat_Pemasangan = $_POST['alamat_pemasangan'];
+// $latitude = $_POST['latitude'];
+// $longitude = $_POST['longitude'];
+// $Email =  $_POST['email'];
+// $Nomor_Hp_1 = $_POST['no_hp1'];
+// $Nomor_Hp_2 = $_POST['no_hp2'];
+// $id_paket = $_POST['id_paket'];
+// $nama_paket = $_POST['nama_paket'];
 
-$query = "UPDATE tb_pengajuan SET 
-          Nama_Lengkap = '$Nama_Lengkap', 
-          Nomor_Identitas_KTP = '$Nomor_Identitas_KTP', 
-          Alamat_Pemasangan = '$Alamat_Pemasangan', 
-          latitude = '$latitude', 
-          longitude = '$longitude', 
-          Email = '$Email', 
-          Nomor_Hp_1 = '$Nomor_Hp_1', 
-          Nomor_Hp_2 = '$Nomor_Hp_2', 
-          id_paket = '$id_paket', 
-          nama_paket = '$nama_paket' 
-          WHERE id_pengajuan = '$id_pengajuan'";
+// $query = "UPDATE tb_pengajuan SET 
+//           Nama_Lengkap = '$Nama_Lengkap', 
+//           Nomor_Identitas_KTP = '$Nomor_Identitas_KTP', 
+//           Alamat_Pemasangan = '$Alamat_Pemasangan', 
+//           latitude = '$latitude', 
+//           longitude = '$longitude', 
+//           Email = '$Email', 
+//           Nomor_Hp_1 = '$Nomor_Hp_1', 
+//           Nomor_Hp_2 = '$Nomor_Hp_2', 
+//           id_paket = '$id_paket', 
+//           nama_paket = '$nama_paket' 
+//           WHERE id_pengajuan = '$id_pengajuan'";
 
-if (mysqli_query($koneksi, $query)) {
-    echo "<script>alert('Data pelanggan berhasil diperbarui.'); window.location.href = 'index.php?page=data-pelanggan';</script>";
-} else {
-    echo "<script>alert('Gagal memperbarui data: " . mysqli_error($koneksi) . "'); history.back();</script>";
-}
+// if (mysqli_query($koneksi, $query)) {
+//     echo "<script>alert('Data pelanggan berhasil diperbarui.'); window.location.href = 'index.php?page=data-pelanggan';</script>";
+// } else {
+//     echo "<script>alert('Gagal memperbarui data: " . mysqli_error($koneksi) . "'); history.back();</script>";
+// }
 ?>
